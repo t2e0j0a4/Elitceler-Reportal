6f5820627b79135ff5c0e435c8cd07e4c433a46f
Valiveti Nagateja <109240118+t2e0j0a4@users.noreply.github.com>
2024-10-30 14:05:26 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from "next/server";
 
 // Utils
-import createToast from './utils/createToast';
 import { getAuthToken } from './utils/getAuthToken';
 
 export default async function middleware(request: NextRequest) {
@@ -12,7 +11,6 @@ export default async function middleware(request: NextRequest) {
   // Check for dashboard paths and missing auth token
   if (urlPath.startsWith('/dashboard')) {
     if (authToken === undefined || !authToken) {
-      createToast('error', 'Login expired!');
       return NextResponse.redirect(new URL('/', request.url));
     }
   }
